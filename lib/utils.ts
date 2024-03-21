8f2640511e40cc64b67e2a389a13dfa1cfd7359f
YashwanthK <yashwanthkothakota@gmail.com>
2024-03-21 11:57:42 +0530

    modified og

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 export function constructMetadata({
   title = "Yashwanth Kothakota's Portfolio",
   description = 'I am a full stack developer from India',
-  image = '/og.png',
+  image = 'https://yashwanth-portfolio.vercel.app/og.png',
   noIndex = false,
 }: {
   title?: string;
